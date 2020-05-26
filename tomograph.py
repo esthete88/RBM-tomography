707f10e059d4c50f381cfdd9caac45a4a200d7e7
esthete88 <nordmtr@gmail.com>
2020-05-26 18:31:16 +0300

    add docs and fix types bug

--- tomograph.py
+++ tomograph.py
@@ -9,7 +9,32 @@ from rbm import RBM
 
 
 class Tomograph(nn.Module):
-    def __init__(self, vis_size, hid_size, gibbs=True, n_samples=1, n_gibbs_steps=1,
+    """Class for RBM tomography.
+
+    Parameters
+    ----------
+    vis_size : int
+        Number of visible neurons (number of qubits).
+    hid_size : int
+        Number of hidden neurons.
+    gibbs : bool
+        If `True`, uses contrastive divergence with Gibbs sampling.
+    n_samples : int
+        Number of samples to use in one iteration.
+    n_gibbs_steps : int
+        Number of steps in each Gibbs chain.
+    init_sigma : float
+        Description of parameter `init_sigma`.
+
+    Attributes
+    ----------
+    amplitude_rbm : RBM
+        RBM for computing amplitude.
+    phase_rbm : RBM
+        RBM for computing phase.
+
+    """
+    def __init__(self, vis_size, hid_size, gibbs=True, n_samples=2, n_gibbs_steps=1,
                  init_sigma=1, dtype=torch.float32, eps=1e-8):
         super().__init__()
 
@@ -48,14 +73,12 @@ class Tomograph(nn.Module):
         vis = idx2vis(fock_indices, self.vis_size)
 
         amplitude_prob = self.amplitude_rbm.prob(vis)
-        amplitude = (torch.sqrt(amplitude_prob / amplitude_prob.sum())).cpu().detach().numpy()
+        amplitude = (torch.sqrt(amplitude_prob / amplitude_prob.sum()))
 
         phase_prob = self.phase_rbm.prob(vis)
-        phase = (torch.log(phase_prob + self._eps) / 2).cpu().detach().numpy()
+        phase = (torch.log(phase_prob + self._eps) / 2)
 
-        predicted_state = amplitude * np.exp(1j * phase)
-
-        return predicted_state
+        return amplitude, phase
 
     def loss(self, data_states, predicted_state):
         data_amplitudes, data_phases = data_states  # [batch_size, n_indices]
@@ -69,7 +92,7 @@ class Tomograph(nn.Module):
 
         return -torch.mean(torch.log(likelihood + self._eps))
 
-    def fit(self, x, theta, n_epochs=1000, lr=1e-3, callbacks=None):
+    def fit(self, x, theta, n_epochs=1000, lr=1e-2, callbacks=None):
         device = next(self.parameters()).device
 
         fock_indices = torch.arange(2 ** self.vis_size)
@@ -77,10 +100,12 @@ class Tomograph(nn.Module):
 
         vis = idx2vis(fock_indices[torch.randint(len(fock_indices), (self.n_samples,))], self.vis_size)
 
-        opt = torch.optim.Adam(self.parameters(), lr=lr)
+        opt = torch.optim.Adam(self.parameters(), lr=lr, betas=(0.9, 0.99))
         for e in range(n_epochs):
             predicted_state, vis = self.forward(vis)
             sampled_indices = vis2idx(vis)
+
+            print(sampled_indices)
 
             data_amplitudes = encoded_data[0][:, sampled_indices]
             data_phases = encoded_data[1][:, sampled_indices]
@@ -98,17 +123,39 @@ class Tomograph(nn.Module):
                 'loss': loss.cpu().detach().numpy(),
             }
 
-            for callback in callbacks:
-                callback(epoch_log)
+            if callbacks is not None:
+                for callback in callbacks:
+                    callback(epoch_log)
 
 
 def encode_data(fock_indices, x, theta, dtype=torch.float32, device=torch.device('cpu')):
-    """Computes <n|X, theta> using Hermitian polynomes."""
+    """Computes <n|X, theta> using Hermitian polynomes.
+
+    Parameters
+    ----------
+    fock_indices : torch.Tensor
+        Indices of fock vectors to use in decomposition.
+    x : torch.Tensor
+        X quadratures of the data.
+    theta : torch.Tensor
+        Theta quadratures of the data.
+
+    Returns
+    -------
+    Tuple of amplitudes and phases.
+
+    """
+
     amplitude = count_hermvals(fock_indices, x) \
               * torch.exp(-x ** 2 / 2).unsqueeze(1) \
-              * torch.pow(2, -fock_indices / 2).unsqueeze(0) \
-              / torch.sqrt(factorial(fock_indices)).unsqueeze(0) \
+              / torch.sqrt(2 ** fock_indices * factorial(fock_indices)).unsqueeze(0) \
               / math.pi ** 0.25
+
+    # amplitude = count_hermvals(fock_indices, x) \
+    #           * torch.exp(-x ** 2 / 2).unsqueeze(1) \
+    #           * torch.pow(2, -fock_indices / 2).unsqueeze(0) \
+    #           / torch.sqrt(factorial(fock_indices)).unsqueeze(0) \
+    #           / math.pi ** 0.25
 
     phase = theta.unsqueeze(1) * fock_indices.unsqueeze(0)
     return amplitude, phase
@@ -127,8 +174,8 @@ def vis2idx(vis):
 
 
 def count_hermvals(n, x, dtype=torch.float32, device=torch.device('cpu')):
-    batch_size = len(x)
-    hermvals = torch.zeros(batch_size, n.shape[0])
+    """Returns tensor of shape [len(x), len(n)] with hermitian polynomes values H_n(x)."""
+    hermvals = torch.zeros(len(x), len(n))
     for i, nval in enumerate(n):
         coef = np.zeros(n[-1] + 1)
         coef[nval] = 1
@@ -137,4 +184,5 @@ def count_hermvals(n, x, dtype=torch.float32, device=torch.device('cpu')):
 
 
 def factorial(x):
+    """Returns element-wise factorial."""
     return torch.exp(torch.lgamma(x + 1.))
