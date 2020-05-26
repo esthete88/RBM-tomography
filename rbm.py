d968a36e0cbe24a97cf2cc00766fc768c0bcba45
esthete88 <nordmtr@gmail.com>
2020-05-26 13:50:28 +0300

    finish mvp

--- rbm.py
+++ rbm.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.distributions import RelaxedBernoulli
+from torch.distributions import Bernoulli
 
 
 class RBM(nn.Module):
@@ -10,31 +10,29 @@ class RBM(nn.Module):
         self.vis_size = vis_size
         self.hid_size = hid_size
 
-        self._W = nn.Parameter(init_sigma * torch.randn(vis_size, hid_size), dtype=dtype)
+        self._W = nn.Parameter(init_sigma * torch.randn(vis_size, hid_size, dtype=dtype))
         self._bv = nn.Parameter(init_sigma * torch.randn(vis_size, dtype=dtype))
         self._bh = nn.Parameter(init_sigma * torch.randn(hid_size, dtype=dtype))
 
-        self.dtype=dtype
+        self.dtype = dtype
 
     def forward_pass(self, vis):
-        n_samples = vis.shape[0]
-        vis = vis.to(dtype=self.dtype)
         hid_logits = vis @ self._W + self._bh
-        bernoulli = RelaxedBernoulli(1, logits=hid_logits)
-        hid = bernoulli.rsample(n_samples)
+        bernoulli = Bernoulli(logits=hid_logits)
+        hid = bernoulli.sample()
         return hid
 
     def backward_pass(self, hid):
-        n_samples = hid.shape[0]
-        vis_logits = self._W @ hid + self._bv
-        bernoulli = RelaxedBernoulli(1, logits=vis_logits)
-        vis = bernoulli.rsample(n_samples)
+        vis_logits = hid @ self._W.T + self._bv
+        bernoulli = Bernoulli(logits=vis_logits)
+        vis = bernoulli.sample()
         return vis
 
-    def sample(self, vis, n_steps=1):
-        for _ in range(n_steps):
-            hid = self.forward_pass(self, vis)
-            vis = self.backward_pass(self, hid)
+    def sample(self, vis, n_gibbs_steps=1):
+        vis = vis.to(dtype=self.dtype)
+        for _ in range(n_gibbs_steps):
+            hid = self.forward_pass(vis)
+            vis = self.backward_pass(hid)
         return vis
 
     def prob(self, vis):
--- tomograph.py
+++ tomograph.py
@@ -9,10 +9,14 @@ from rbm import RBM
 
 
 class Tomograph(nn.Module):
-    def __init__(self, vis_size, hid_size, n_steps=1, init_sigma=1, dtype=torch.float32, eps=1e-8):
+    def __init__(self, vis_size, hid_size, n_samples=1, n_gibbs_steps=1, init_sigma=1, dtype=torch.float32, eps=1e-8):
+        super().__init__()
 
         self.vis_size = vis_size
         self.hid_size = hid_size
+
+        self.n_gibbs_steps = n_gibbs_steps
+        self.n_samples = n_samples
 
         self.amplitude_rbm = RBM(vis_size, hid_size, init_sigma=init_sigma, dtype=dtype)
         self.phase_rbm = RBM(vis_size, hid_size, init_sigma=init_sigma, dtype=dtype)
@@ -20,10 +24,8 @@ class Tomograph(nn.Module):
         self.dtype = dtype
         self._eps = eps
 
-    def forward(self, fock_indices):
-        device = next(self.parameters()).device
-        vis = idx2vis(fock_indices, self.vis_size, dtype=self.dtype, device=device)
-        sampled_vis = self.amplitude_rbm.sample(vis, n_steps=self.n_steps)
+    def forward(self, vis):
+        sampled_vis = self.amplitude_rbm.sample(vis, n_gibbs_steps=self.n_gibbs_steps)
 
         amplitude_prob = self.amplitude_rbm.prob(sampled_vis)
         amplitude = torch.sqrt(amplitude_prob / amplitude_prob.sum())
@@ -31,18 +33,67 @@ class Tomograph(nn.Module):
         phase_prob = self.phase_rbm.prob(sampled_vis)
         phase = torch.log(phase_prob + self._eps) / 2
 
-        return amplitude, phase
+        predicted_state = amplitude, phase
 
-    def loss(self, encoded_data, fock_indices):
-        state_amplitude, state_phase = self(fock_indices)  # [n_indices,]
-        data_amplitude, data_phase = encoded_data  # [batch_size, n_indices]
-        amplitude = data_amplitude * state_amplitude
-        phase = data_phase - state_amplitude
+        return predicted_state, sampled_vis
 
-        likelihood = torch.sum(amplitude * torch.cos(phase), dim=0) ** 2 \
-                   + torch.sum(amplitude * torch.sin(phase), dim=0) ** 2
+    def predict(self):
+        fock_indices = torch.arange(2 ** self.vis_size)
+        vis = idx2vis(fock_indices, self.vis_size)
+
+        amplitude_prob = self.amplitude_rbm.prob(vis)
+        amplitude = (torch.sqrt(amplitude_prob / amplitude_prob.sum())).cpu().detach().numpy()
+
+        phase_prob = self.phase_rbm.prob(vis)
+        phase = (torch.log(phase_prob + self._eps) / 2).cpu().detach().numpy()
+
+        predicted_state = amplitude * np.exp(1j * phase)
+
+        return predicted_state
+
+    def loss(self, data_states, predicted_state):
+        data_amplitudes, data_phases = data_states  # [batch_size, n_indices]
+        predicted_amplitude, predicted_phase = predicted_state  # [n_indices,]
+
+        amplitudes = data_amplitudes * predicted_amplitude
+        phases = data_phases - predicted_amplitude
+
+        likelihood = torch.sum(amplitudes * torch.cos(phases), dim=0) ** 2 \
+                   + torch.sum(amplitudes * torch.sin(phases), dim=0) ** 2
 
         return -torch.mean(torch.log(likelihood + self._eps))
+
+    def fit(self, x, theta, n_epochs=1000, lr=1e-3, callbacks=None):
+        device = next(self.parameters()).device
+
+        fock_indices = torch.arange(2 ** self.vis_size)
+        encoded_data = encode_data(fock_indices, x, theta, self.dtype, device)  # [batch_size, n_indices]
+
+        vis = idx2vis(fock_indices[torch.randint(len(fock_indices), (self.n_samples,))], self.vis_size)
+
+        opt = torch.optim.Adam(self.parameters(), lr=lr)
+        for e in range(n_epochs):
+            predicted_state, vis = self.forward(vis)
+            sampled_indices = vis2idx(vis)
+
+            data_amplitudes = encoded_data[0][:, sampled_indices]
+            data_phases = encoded_data[1][:, sampled_indices]
+            data_states = data_amplitudes, data_phases
+
+            loss = self.loss(data_states, predicted_state)
+
+            opt.zero_grad()
+            loss.backward()
+            opt.step()
+
+            epoch_log = {
+                'epoch': e,
+                'n_epochs': n_epochs,
+                'loss': loss.cpu().detach().numpy(),
+            }
+
+            for callback in callbacks:
+                callback(epoch_log)
 
 
 def encode_data(fock_indices, x, theta, dtype=torch.float32, device=torch.device('cpu')):
@@ -60,24 +111,24 @@ def encode_data(fock_indices, x, theta, dtype=torch.float32, device=torch.device
 def idx2vis(idx, dim, dtype=torch.float32, device=torch.device('cpu')):
     vis = torch.zeros(idx.shape[0], dim, dtype=dtype, device=device)
     for i in range(idx.shape[0]):
-        id_bin = torch.as_tensor([int(c) for c in bin(int(idx[i]))[2:]], dtype=dtype, device=device)
+        id_bin = torch.as_tensor([int(c) for c in bin(idx[i])[2:]], dtype=dtype, device=device)
         vis[i, -id_bin.shape[0]:] = id_bin
     return vis
 
 
 def vis2idx(vis):
-    return torch.sum(2 ** reversed(torch.arange(0, vis.shape[1])) * vis, dim=1)
+    return torch.sum(2 ** reversed(torch.arange(0, vis.shape[1])) * vis, dim=1, dtype=torch.long)
 
 
 def count_hermvals(n, x, dtype=torch.float32, device=torch.device('cpu')):
     batch_size = len(x)
     hermvals = torch.zeros(batch_size, n.shape[0])
     for i, nval in enumerate(n):
-        coef = np.zeros(int(n[-1]) + 1)
+        coef = np.zeros(n[-1] + 1)
         coef[nval] = 1
         hermvals[:, i] = hermval(x, coef)
     return hermvals
 
 
 def factorial(x):
-    return torch.exp(torch.lgamma(x + 1))
+    return torch.exp(torch.lgamma(x + 1.))
