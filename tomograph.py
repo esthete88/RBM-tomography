d6d9ec1e6437030432ec4dcc4e5d891f574d3d42
esthete88 <nordmtr@gmail.com>
2020-05-27 16:28:59 +0300

    fix monte-carlo sampling

--- tomograph.py
+++ tomograph.py
@@ -34,8 +34,9 @@ class Tomograph(nn.Module):
         RBM for computing phase.
 
     """
+
     def __init__(self, vis_size, hid_size, gibbs=True, n_samples=2, n_gibbs_steps=1,
-                 init_sigma=1, dtype=torch.float32, eps=1e-8):
+                 init_sigma=1, dtype=torch.float32, eps=1e-10):
         super().__init__()
 
         self.vis_size = vis_size
@@ -59,14 +60,10 @@ class Tomograph(nn.Module):
             vis = idx2vis(fock_indices, self.vis_size)
 
         amplitude_prob = self.amplitude_rbm.prob(vis)
-        amplitude = torch.sqrt(amplitude_prob / amplitude_prob.sum())
-
         phase_prob = self.phase_rbm.prob(vis)
-        phase = torch.log(phase_prob + self._eps) / 2
+        rbm_probs = amplitude_prob, phase_prob
 
-        predicted_state = amplitude, phase
-
-        return predicted_state, vis
+        return rbm_probs, vis
 
     def predict(self):
         fock_indices = torch.arange(2 ** self.vis_size)
@@ -80,19 +77,29 @@ class Tomograph(nn.Module):
 
         return amplitude, phase
 
-    def loss(self, data_states, predicted_state):
-        data_amplitudes, data_phases = data_states  # [batch_size, n_indices]
-        predicted_amplitude, predicted_phase = predicted_state  # [n_indices,]
+    def llh_loss(self, encoded_data, rbm_probs):
+        data_amplitudes, data_phases = encoded_data  # [batch_size, n_indices]
+        amplitude_prob, phase_prob = rbm_probs  # [n_indices,]
 
-        amplitudes = data_amplitudes * predicted_amplitude
-        phases = data_phases - predicted_phase
+        unnorm_predicted_amplitude = torch.sqrt(amplitude_prob)
+        predicted_phase = torch.log(phase_prob + self._eps) / 2
 
-        likelihood = torch.sum(amplitudes * torch.cos(phases), dim=1) ** 2 \
-                   + torch.sum(amplitudes * torch.sin(phases), dim=1) ** 2
+        amplitudes = data_amplitudes * unnorm_predicted_amplitude
+        phases = data_phases + predicted_phase
 
-        return -torch.mean(torch.log(likelihood + self._eps))
+        data_term = torch.sum(amplitudes * torch.cos(phases), dim=1) ** 2 \
+                  + torch.sum(amplitudes * torch.sin(phases), dim=1) ** 2
 
-    def fit(self, x, theta, n_epochs=1000, lr=1e-2, callbacks=None):
+        if self.gibbs:
+            normalization_term = torch.mean(torch.log(amplitude_prob + self._eps))
+        else:
+            normalization_term = torch.log(amplitude_prob.sum() + self._eps)
+
+        llh = torch.mean(torch.log(data_term + self._eps)) - normalization_term
+
+        return -llh
+
+    def fit(self, x, theta, n_epochs=1000, lr=1e-1, callbacks=None):
         device = next(self.parameters()).device
 
         fock_indices = torch.arange(2 ** self.vis_size)
@@ -102,14 +109,14 @@ class Tomograph(nn.Module):
 
         opt = torch.optim.Adam(self.parameters(), lr=lr, betas=(0.9, 0.99))
         for e in range(n_epochs):
-            predicted_state, vis = self.forward(vis)
+            rbm_probs, vis = self.forward(vis)
             sampled_indices = vis2idx(vis)
 
             data_amplitudes = encoded_data[0][:, sampled_indices]
             data_phases = encoded_data[1][:, sampled_indices]
             data_states = data_amplitudes, data_phases
 
-            loss = self.loss(data_states, predicted_state)
+            loss = self.llh_loss(data_states, rbm_probs)
 
             opt.zero_grad()
             loss.backward()
@@ -148,12 +155,6 @@ def encode_data(fock_indices, x, theta, dtype=torch.float32, device=torch.device
               * torch.exp(-x ** 2 / 2).unsqueeze(1) \
               / torch.sqrt(2 ** fock_indices * factorial(fock_indices)).unsqueeze(0) \
               / math.pi ** 0.25
-
-    # amplitude = count_hermvals(fock_indices, x) \
-    #           * torch.exp(-x ** 2 / 2).unsqueeze(1) \
-    #           * torch.pow(2, -fock_indices / 2).unsqueeze(0) \
-    #           / torch.sqrt(factorial(fock_indices)).unsqueeze(0) \
-    #           / math.pi ** 0.25
 
     phase = theta.unsqueeze(1) * fock_indices.unsqueeze(0)
     return amplitude, phase
